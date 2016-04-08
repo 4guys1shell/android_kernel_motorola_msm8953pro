2d75e1f60e78f7d9717a66b70ee00892088e9cd8
Mathias Nyman <mathias.nyman@linux.intel.com>
2016-04-08 16:25:10 +0300

    xhci: fix 10 second timeout on removal of PCI hotpluggable xhci controllers
    [ Upstream commit 98d74f9ceaefc2b6c4a6440050163a83be0abede ]
    
    PCI hotpluggable xhci controllers such as some Alpine Ridge solutions will
    remove the xhci controller from the PCI bus when the last USB device is
    disconnected.
    
    Add a flag to indicate that the host is being removed to avoid queueing
    configure_endpoint commands for the dropped endpoints.
    For PCI hotplugged controllers this will prevent 5 second command timeouts
    For static xhci controllers the configure_endpoint command is not needed
    in the removal case as everything will be returned, freed, and the
    controller is reset.
    
    For now the flag is only set for PCI connected host controllers.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    
    Conflicts:
    	drivers/usb/host/xhci.c

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -3835,12 +3835,10 @@ static int xhci_setup_device(struct usb_hcd *hcd, struct usb_device *udev,
 	u64 temp_64;
 	struct xhci_command *command = NULL;
 
-	mutex_lock(&xhci->mutex);
+	if (xhci->xhc_state) /* dying, removing  or halted  */
+		return -EINVAL;
 
-	if (xhci->xhc_state)	/* dying, removing or halted */
-		goto out;
-
-	if (!udev->slot_id) {
+	if(!udev->slot_id) {
 		xhci_dbg_trace(xhci, trace_xhci_dbg_address,
 				"Bad Slot ID %d", udev->slot_id);
 		ret = -EINVAL;
